e6cc5a96f5c00b5ca68b1eba6c1c983f8c9b6d09
ishor2017 <alemagarishor@gmail.com>
2018-09-08 09:22:59 +0545

    first comit

---
+++ classMain.js
@@ -0,0 +1,31 @@
+//Multiplyy
+/*var a = 3;
+var b = 4;
+var mul = a * b; 
+console.log( mul );*/
+
+//Print Hello name
+//var myname = 'Ishor Ale Magar';
+
+/*alert(myname)
+console.log('Hello ' + myname);
+document.write('Hello:' + myname);*/
+
+//Loop
+/*debugger
+for(i = 0; i < 5; i++){
+	document.write('Hello:' + myname + '<br/>');
+}*/
+
+/*var react = 'React';
+for( i = 1; i <= 1000; i++ ){
+	document.write( i + ': ' + react + '<br/>' );
+	console.log(i);
+}
+*/
+// while dowhile
+
+function yourname(){
+	var nameval = document.getElementById('name').value;
+	document.getElementById('print-ele').innerHTML = 'Hi, ' + nameval;
+}
